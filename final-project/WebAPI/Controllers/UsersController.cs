9717992ab0358e75e2db91b505f3d298c53926ca
Gil Shechter <gilshechter13@gmail.com>
2023-02-28 19:29:40 +0200

    Creating the WebAPI project

---
+++ final-project/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _dbContext;
+
+        public UsersController(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            if (_dbContext.Users == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            if (_dbContext.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _dbContext.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
---
+++ final-project/WebAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPI.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public bool isTeacher { get; set; }
+        public override string ToString()
+        {
+            return this.Name;
+        }
+        public User(string name, string id, bool isTeacher)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.isTeacher = isTeacher;
+        }
+    }
+}
