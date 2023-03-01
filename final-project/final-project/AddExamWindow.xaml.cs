81d35f55cb557e6a078458165a5e8ce84332bad3
Amit Finzi <finzi.amit@gmail.com>
2023-03-02 00:52:58 +0200

    Fixing 'add question button' and adding scrollbar

--- final-project/final-project/AddExamWindow.xaml.cs
+++ final-project/final-project/AddExamWindow.xaml.cs
@@ -71,7 +71,7 @@ namespace final_project
             /*
              * Button to add another question to the exam and show its empty fields.
              */
-            string answerContent;
+            Answer answer;
 
             int i = ListBoxQuestions.Items.Count;
             string name = "Question " + i.ToString();
@@ -79,27 +79,48 @@ namespace final_project
             // update the questions count
             QuestionsNumberBox.Text = (i+1).ToString();
 
-            // add a new question to the list
-            ListBoxQuestions.Items.Add(name);
-
             /*
              * add all the fields to the last question in our questions list
              * and clear the answers list
              */
             string questionContent = ContentTxt.Text;
             
-            // not finished! this window's functionallity still needs some work
             for(int j=0; j<this.OptionalAnswers.Children.Count; j++)
             {
-                if(j%2 == 0) // the items in even places are the answer content
+                answer = new Answer(string.Empty);
+
+                if (OptionalAnswers.Children[j] is TextBox)
                 {
-                    answerContent = OptionalAnswers.Children[j].Text;
+                    TextBox tb = (TextBox)OptionalAnswers.Children[j];
+                    if (tb != null)
+                    {
+                        answer.content = tb.Text;
+                    }
+                }
+
+                else if (OptionalAnswers.Children[j] is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)OptionalAnswers.Children[j];
+                    if (cb.IsChecked == true)
+                    {
+                        answer.correct_answer = true;
+                    }
+                }
+
+                if (answer.content != string.Empty)
+                {
+                    this._answers.Add(answer);
                 }
             }
 
+            Question question = new Question(questionContent, this._answers.ToArray());
+            question.Id = ListBoxQuestions.Items.Count + 1;
+            this._questions.Add(question);
 
+            ListBoxQuestions.Items.Add((Question)question);
+            this._answers.Clear();
             // select the new question
-            ListBoxQuestions.SelectedIndex = i+1;
+            ListBoxQuestions.SelectedIndex = i;
         }
 
         private void AddAnswerBtn_Click(object sender, RoutedEventArgs e)
@@ -107,7 +128,13 @@ namespace final_project
             /*
              * Button to add another Answer to the exam and show its empty field
              */
-            TextBlock tb = new TextBlock();
+
+            if (ListBoxQuestions.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            TextBox tb = new TextBox();
             CheckBox cb = new CheckBox();
             int numOfQuestions = (OptionalAnswers.Children.Count / 2)+1;
             string name = "Answer"+numOfQuestions.ToString();
@@ -115,6 +142,7 @@ namespace final_project
             tb.Margin=new Thickness(10);
             tb.HorizontalAlignment = HorizontalAlignment.Left;
             tb.Width = 400;
+            tb.Background = Brushes.LightGray;
             tb.Name = name;
 
             cb.Content = "Mark as correct answer";
